3e76a43f2bff121f9c9a27700d620e941c2a2070
younesnarjis <44195360+younesnarjis@users.noreply.github.com>
2018-10-23 13:33:32 +0200

    interfaz grafica

--- Invasion/src/Modelo/Vampiro.java
+++ Invasion/src/Modelo/Vampiro.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Vista;
+package Modelo;
 
 /**
  *
- * @author YUYAN
+ * @author youne
  */
-public class NewClass {
+public class Vampiro {
     
 }
