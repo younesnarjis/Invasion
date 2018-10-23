56be893f9dae40f33b87a295716f87c6fab16a4f
younesnarjis <44195360+younesnarjis@users.noreply.github.com>
2018-10-23 14:02:46 +0200

    nn

--- Invasion/src/Modelo/Vampiro.java
+++ Invasion/src/Modelo/Vampiro.java
@@ -9,6 +9,19 @@ package Modelo;
  *
  * @author youne
  */
-public class Vampiro {
+public class Vampiro extends Seres{
+    private int numVampGenerados;
+    private static int id_aprox;
+ 
+    
+    public Vampiro(){
+        super
+        this.numVampGenerados = 0;
+    }
+    public 
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
